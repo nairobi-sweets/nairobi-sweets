96bb1f884249006bfdce72cac52a17f0b746ce1e
nairobi-sweets <nairobisweets05@gmail.com>
2026-04-22 11:10:50 +0300

    Update test-alive.js

--- netlify/functions/test-alive.js
+++ netlify/functions/test-alive.js
@@ -2,6 +2,9 @@ exports.handler = async () => {
   return {
     statusCode: 200,
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ ok: true, message: "Functions are alive" }),
+    body: JSON.stringify({
+      ok: true,
+      message: "Functions are alive"
+    }),
   };
 };
