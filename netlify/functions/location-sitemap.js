ae5a0d850ab1a8d6f0111aeadacaabc38af27d09
nairobi-sweets <nairobisweets05@gmail.com>
2026-06-06 10:41:26 +0300

    Update location-sitemap.js

--- netlify/functions/location-sitemap.js
+++ netlify/functions/location-sitemap.js
@@ -1,43 +1,59 @@
 exports.handler = async () => {
+  const locations = [
+    "nairobi",
+    "kilimani",
+    "westlands",
+    "lavington",
+    "ngong-road",
+    "embakasi",
+    "south-b",
+    "south-c",
+    "parklands",
+    "karen",
+    "rongai",
+    "kitengela",
+    "kasarani",
+    "roysambu",
+    "ruaka",
+    "ruiru",
+    "kiambu",
+    "thika",
+    "juja",
+    "zimmerman",
+    "mirema",
+    "trm",
+    "githurai",
+    "donholm",
+    "umoja",
+    "buruburu",
+    "fedha",
+    "syokimau",
+    "athi-river",
+    "thindigua",
+    "kiambu-road",
+    "kahawa-west",
+    "kahawa-sukari",
+    "juja-farm",
+    "kimbo"
+  ];
 
-const locations = [
-  "nairobi",
-  "kilimani",
-  "westlands",
-  "lavington",
-  "ngong-road",
-  "embakasi",
-  "south-b",
-  "south-c",
-  "parklands",
-  "kasarani",
-  "ruaka",
-  "ruiru",
-  "kiambu",
-  "thika",
-  "juja",
-  "kitengela",
-  "rongai",
-  "karen",
-  "roysambu"
-];
+  const urls = locations.map((loc) => {
+    return `  <url>
+    <loc>https://nairobi-sweets.com/seo/locations/${loc}.html</loc>
+    <changefreq>daily</changefreq>
+    <priority>0.8</priority>
+  </url>`;
+  }).join("\n");
 
-const urls = locations.map(loc => `
-<url>
-  <loc>https://nairobi-sweets.com/seo/locations/${loc}.html</loc>
-  <changefreq>daily</changefreq>
-  <priority>0.8</priority>
-</url>`).join("");
-
-return {
-  statusCode: 200,
-  headers: {
-    "Content-Type": "application/xml"
-  },
-  body: `<?xml version="1.0" encoding="UTF-8"?>
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/xml; charset=utf-8",
+      "Cache-Control": "public, max-age=300"
+    },
+    body: `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${urls}
 </urlset>`
-};
-
+  };
 };
