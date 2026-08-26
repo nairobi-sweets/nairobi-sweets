a8e949e4763692d1e8a25e651c2b6752c8140303
nairobi-sweets <nairobisweets05@gmail.com>
2026-08-26 12:49:24 +0300

    Change query parameter type from 'locations' to 'profiles'

--- netlify/functions/dynamic-profile-sitemap.js
+++ netlify/functions/dynamic-profile-sitemap.js
@@ -3,7 +3,7 @@ const generate = require("./generate-sitemaps.js");
 exports.handler = async function (event, context) {
   event.queryStringParameters = {
     ...(event.queryStringParameters || {}),
-    type: "locations"
+    type: "profiles"
   };
 
   return generate.handler(event, context);
